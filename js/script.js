187e3c6c2db6a632f89ecc7fe2346ac91aba0dfc
Favour <favourOL1@outlook.com>
2024-03-22 09:55:59 +0100

    My UBA project

---
+++ js/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    const navbar=document.getElementById("navbar")
+
+    document.addEventListener('scroll',()=>{
+
+        const scrollY = window.scrollY
+
+        if(scrollY > 100) {
+            navbar.classList.add('white-nav')
+        } else{
+            navbar.classList.remove('white-nav')
+        }
+
+        console.log(window.scrollY)
+
+    })
+
+
+})
